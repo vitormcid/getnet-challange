35e928897db4cbd221bcbb77cc35ba2c87fcc881
Vitor Cid <cid.vitor@hotmail.com>
2021-12-01 21:20:32 -0300

    added the rest of the tests to cover all the users crud

--- main_test.go
+++ main_test.go
@@ -2,10 +2,13 @@
 package main_test
 
 import (
+	"bytes"
+	"encoding/json"
 	"log"
 	"net/http"
 	"net/http/httptest"
 	"os"
+	"strconv"
 	"testing"
         "github.com/vitormcid/getnet-challange"
 )
@@ -38,6 +41,128 @@ func TestEmptyTable(t *testing.T) {
 	}
 }
 
+func TestGetNonExistentUser(t *testing.T) {
+	clearTable()
+
+	req, _ := http.NewRequest("GET", "/user/11", nil)
+	response := executeRequest(req)
+
+	checkResponseCode(t, http.StatusNotFound, response.Code)
+
+	var m map[string]string
+	json.Unmarshal(response.Body.Bytes(), &m)
+	if m["error"] != "User not found" {
+		t.Errorf("Expected the 'error' key of the response to be set to 'User not found'. Got '%s'", m["error"])
+	}
+}
+
+func TestCreateUser(t *testing.T) {
+
+	clearTable()
+
+	var jsonStr = []byte(`{"name":"test user", "email": "user@teste.com", "password" "password"}`)
+	req, _ := http.NewRequest("POST", "/user", bytes.NewBuffer(jsonStr))
+	req.Header.Set("Content-Type", "application/json")
+
+	response := executeRequest(req)
+	checkResponseCode(t, http.StatusCreated, response.Code)
+
+	var m map[string]interface{}
+	json.Unmarshal(response.Body.Bytes(), &m)
+
+	if m["name"] != "test user" {
+		t.Errorf("Expected user name to be 'test user'. Got '%v'", m["name"])
+	}
+
+	if m["email"] != "user@teste.com" {
+		t.Errorf("Expected user price to be user@teste.com. Got '%v'", m["email"])
+	}
+
+	//to do password
+
+	// the id is compared to 1.0 because JSON unmarshaling converts numbers to
+	// floats, when the target is a map[string]interface{}
+	if m["id"] != 1.0 {
+		t.Errorf("Expected user ID to be '1'. Got '%v'", m["id"])
+	}
+}
+
+func TestGetUser(t *testing.T) {
+	clearTable()
+	addUsers(1)
+
+	req, _ := http.NewRequest("GET", "/user/1", nil)
+	response := executeRequest(req)
+
+	checkResponseCode(t, http.StatusOK, response.Code)
+}
+
+func addUsers(count int) {
+	if count < 1 {
+		count = 1
+	}
+
+	for i := 0; i < count; i++ {
+		a.DB.Exec("INSERT INTO users(name, email, password) VALUES($1, $2, $3)", "User "+strconv.Itoa(i), "User@"+strconv.Itoa(i), "password")
+	}
+}
+
+func TestUpdateUser(t *testing.T) {
+
+	clearTable()
+	addUsers(1)
+
+	req, _ := http.NewRequest("GET", "/user/1", nil)
+	response := executeRequest(req)
+	var originalUser map[string]interface{}
+	json.Unmarshal(response.Body.Bytes(), &originalUser)
+
+	var jsonStr = []byte(`{"name":"test user - updated name", "email": "user@teste.com", "password": "password"}`)
+	req, _ = http.NewRequest("PUT", "/user/1", bytes.NewBuffer(jsonStr))
+	req.Header.Set("Content-Type", "application/json")
+
+	response = executeRequest(req)
+
+	checkResponseCode(t, http.StatusOK, response.Code)
+
+	var m map[string]interface{}
+	json.Unmarshal(response.Body.Bytes(), &m)
+
+	if m["id"] != originalUser["id"] {
+		t.Errorf("Expected the id to remain the same (%v). Got %v", originalUser["id"], m["id"])
+	}
+
+	if m["name"] == originalUser["name"] {
+		t.Errorf("Expected the name to change from '%v' to '%v'. Got '%v'", originalUser["name"], m["name"], m["name"])
+	}
+
+	if m["email"] == originalUser["email"] {
+		t.Errorf("Expected the email to change from '%v' to '%v'. Got '%v'", originalUser["email"], m["email"], m["email"])
+	}
+
+	if m["password"] == originalUser["password"] {
+		t.Errorf("Expected the password to change from '%v' to '%v'. Got '%v'", originalUser["password"], m["password"], m["password"])
+	}
+}
+
+func TestDeleteUser(t *testing.T) {
+	clearTable()
+	addUsers(1)
+
+	req, _ := http.NewRequest("GET", "/user/1", nil)
+	response := executeRequest(req)
+	checkResponseCode(t, http.StatusOK, response.Code)
+
+	req, _ = http.NewRequest("DELETE", "/user/1", nil)
+	response = executeRequest(req)
+
+	checkResponseCode(t, http.StatusOK, response.Code)
+
+	req, _ = http.NewRequest("GET", "/user/1", nil)
+	response = executeRequest(req)
+	checkResponseCode(t, http.StatusNotFound, response.Code)
+}
+
 func executeRequest(req *http.Request) *httptest.ResponseRecorder {
 	rr := httptest.NewRecorder()
 	a.Router.ServeHTTP(rr, req)
@@ -66,6 +191,7 @@ const tableCreationQuery = `CREATE TABLE IF NOT EXISTS users
 (
     id SERIAL,
     name TEXT NOT NULL,
-    price NUMERIC(10,2) NOT NULL DEFAULT 0.00,
+    email TEXT NOT NULL,
+	password TEXT NOT NULL,
     CONSTRAINT users_pkey PRIMARY KEY (id)
 )`
